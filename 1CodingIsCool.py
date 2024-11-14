056edc9428c4582cbb4eec1131269292ce23b790
Juliana Yamsuan <juliana.yamsuan@gmail.com>
2024-11-14 23:14:07 +0800

    Exercise 1 COMPLETED

---
+++ 1CodingIsCool.py
@@ -0,0 +1,5 @@
+# Using string concatenation to combine the variables and print the phrase
+string1 = "Coding " # Space is given after the word so that the words won't stick together in the final phrase
+string2 = "is "
+string3 = "cool "
+print(string1 + string2 + string3) # The final phrase from the three string above
