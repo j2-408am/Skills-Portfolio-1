8d90a3bcace1ea9031753e9d5125ebf72575b79b
Juliana Yamsuan <juliana.yamsuan@gmail.com>
2024-11-14 23:16:46 +0800

    Exercise 3 COMPLETED

---
+++ 3Biography.py
@@ -0,0 +1,8 @@
+#Below are my dictionary variables. Note: Any variable can be used.
+#In this case, the variables are related to my information.
+Dict = {
+    'Username': 'Ju Yams', #For the keys, I decided to use strings: username, hometown, and age so that the information is properly labelled.
+    'Hometown': 'Laguna',
+    'Age': '22'
+}
+print(Dict) #A single code to print all related information needed.
