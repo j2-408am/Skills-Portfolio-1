db20a9404c686c3a703ed7cfa0100821147561e0
Juliana Yamsuan <juliana.yamsuan@gmail.com>
2024-11-14 23:15:57 +0800

    Exercise 2 COMPLETE

---
+++ 2SimpleSums.py
@@ -0,0 +1,5 @@
+# Using two variables and adding them to get the sum
+x = 8
+y = 10
+sum = x + y # The operation to get the sum of the two variables
+print("The value of the sum:", sum) # Final phrase after finally computing for the sum of the two variables
